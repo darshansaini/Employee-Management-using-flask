7f149720f1bfe2927c1da0873dd80fe7ac18e045
darshansaini <darshansaini683@gmail.com>
2021-09-21 14:31:15 -0700

    Employee Management using flask

---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "Secret Key"
+
+
+# SqlAlchemy Database Configuration With Mysql
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/EmpMan'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+# Creating model table for our EmpMan database
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(10))
+    salary = db.Column(db.Integer)
+    department = db.Column(db.String(100))
+
+    def __init__(self, name, email, phone, salary, department):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.salary = salary
+        self.department = department
+
+
+# This is the index route where we are going to
+# query on all our employee data
+@app.route('/')
+def Index():
+    all_data = Employee.query.order_by(Employee.id.desc()).all()
+
+    return render_template("index.html", employees=all_data)
+
+
+# this route is for inserting data to mysql database via html forms
+@app.route('/insert', methods=['POST'])
+def insert():
+    name = request.form['name']
+    email = request.form['email']
+    phone = request.form['phone']
+    salary = request.form['salary']
+    department = request.form['department']
+
+    my_data = Employee(name, email, phone, salary, department)
+    db.session.add(my_data)
+    db.session.commit()
+
+    flash("Employee Inserted Successfully")
+
+    return redirect(url_for('Index'))
+
+
+# this is our update route where we are going to update our employee
+@app.route('/update', methods=['POST'])
+def update():
+
+    my_data = Employee.query.get(request.form.get('id'))
+
+    my_data.name = request.form['name']
+    my_data.email = request.form['email']
+    my_data.phone = request.form['phone']
+    my_data.salary = request.form['salary']
+    my_data.department = request.form['department']
+
+    db.session.commit()
+    flash("Employee Updated Successfully")
+
+    return redirect(url_for('Index'))
+
+
+# This route is for deleting our employee
+@app.route('/delete/<id>', methods=['GET','POST'])
+def delete(id):
+    my_data = Employee.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Employee Deleted Successfully")
+
+    return redirect(url_for('Index'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
